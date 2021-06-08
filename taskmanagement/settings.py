028d4637f4d30b9881cf1ad52d63caf5a872da29
NandiniK <nandini.kasu@hashstudioz.com>
2021-06-08 19:18:50 +0530

    rest settings added

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
